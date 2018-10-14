2de28f501c5ba9de7ea4429df64843b19c2ef7d2
jarema5050 <jarema5050@users.noreply.github.com>
2018-10-14 12:59:25 +0200

    initial project

---
+++ fractions/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace fractions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(163, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 28);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(163, 181);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 28);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(363, 124);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(35, 28);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(363, 181);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(35, 28);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(522, 124);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(35, 28);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox6.Location = new System.Drawing.Point(522, 181);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(35, 28);
+            this.textBox6.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "------";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(360, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "--------";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(524, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "-----";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(300, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Dodawanie";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(300, 306);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Odejmowanie";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(300, 333);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Mnożenie";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(300, 362);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Dzielenie";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox7.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox7.Location = new System.Drawing.Point(236, 158);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(40, 28);
+            this.textBox7.TabIndex = 19;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(437, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 24);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "=";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox8.Location = new System.Drawing.Point(464, 157);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(35, 28);
+            this.textBox8.TabIndex = 21;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox9.Location = new System.Drawing.Point(309, 158);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(35, 28);
+            this.textBox9.TabIndex = 22;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox10.Location = new System.Drawing.Point(99, 158);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(35, 28);
+            this.textBox10.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Fractions Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ fractions/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fractions
+{
+    public struct fraction
+    {
+
+        public int numerator;
+        public int denominator;
+        public int newnum;
+        public int newdenum;
+        public int bignum;
+        public int nwd(int a, int b)
+        {
+            b = Math.Abs(b);
+            if (a > 0 & b > 0)
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                return a;
+
+            }
+            else
+            { 
+                return 1;
+            }
+        }
+        public void add(int num, int denum, int num2, int denum2)
+        {
+            if (denum == denum2)
+            {
+                newnum = num + num2;
+                newdenum = denum;
+            }
+            else
+            {
+
+                newdenum = denum * denum2;
+                newnum = denum * num2 + denum2 * num;
+            }
+            if (newnum != 0)
+            {
+                bignum = newnum / newdenum;
+                if (bignum < 0)
+                    newnum = (newnum % newdenum) * (-1);
+                else
+                    newnum = newnum % newdenum;
+            }
+            int nwdd = nwd(newdenum, newnum);
+            if (nwdd != 1)
+            {
+                newdenum = newdenum / nwdd;
+                newnum = newnum / nwdd;
+            }
+        }
+        public void subtract(int num, int denum, int num2, int denum2)
+        {
+            if (denum == denum2)
+            {
+                newnum = num - num2;
+                newdenum = denum;
+            }
+            else
+            {
+                newdenum = denum * denum2;
+                newnum = denum2 * num - denum*num2;
+            }
+            if (newnum != 0)
+            {
+                bignum = newnum /newdenum;
+                if(bignum<0)
+                    newnum = (newnum % newdenum) * (-1);
+                else
+                    newnum = newnum % newdenum;
+            }
+            int nwdd = nwd(newdenum, newnum);
+            if (nwdd != 1)
+            {
+                newdenum = newdenum / nwdd;
+                newnum = newnum / nwdd;
+            }
+        }
+        public void multi(int num, int denum, int num2, int denum2)
+        {
+            newnum = num * num2;
+            newdenum = denum * denum2;
+            if (newnum != 0)
+            {
+                bignum = newnum / newdenum;
+                if (bignum < 0)
+                    newnum = (newnum % newdenum) * (-1);
+                else
+                    newnum = newnum % newdenum;
+            }
+            int nwdd = nwd(newdenum, newnum);
+            if (nwdd != 1)
+            {
+                newdenum = newdenum / nwdd;
+                newnum = newnum / nwdd;
+            }
+        }
+        public void divis(int num, int denum, int num2, int denum2)
+        {
+            newnum = num * denum2;
+            newdenum = denum * num2;
+            if (newnum != 0)
+            {
+                bignum = newnum / newdenum;
+                if (bignum < 0)
+                    newnum = (newnum % newdenum) * (-1);
+                else
+                    newnum = newnum % newdenum;
+            }
+            int nwdd = nwd(newdenum, newnum);
+            if (nwdd != 1)
+            {
+                newdenum = newdenum / nwdd;
+                newnum = newnum / nwdd;
+            }
+        }
+    }
+    public partial class Form1 : Form
+    {
+        fraction first = new fraction();
+        fraction second = new fraction();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void input()
+        {
+            int output1, output2;
+            int.TryParse(textBox10.Text, out output1);
+            int.TryParse(textBox9.Text, out output2);
+            if (output1!=0)
+                {
+                first.numerator = int.Parse(textBox1.Text);
+                first.denominator = int.Parse(textBox2.Text);
+                first.numerator = first.numerator + (output1*first.denominator);
+            }
+            else
+            {
+                    first.numerator = int.Parse(textBox1.Text);
+                    first.denominator = int.Parse(textBox2.Text);
+            }
+            if (output2!=0)
+            {
+                second.numerator = int.Parse(textBox3.Text);
+                second.denominator = int.Parse(textBox4.Text);
+                second.numerator = second.numerator+(output2*second.denominator);
+            }
+           else
+            { 
+                second.numerator = int.Parse(textBox3.Text);
+                second.denominator = int.Parse(textBox4.Text);
+            }
+        }
+        public void clear()
+        {   if (textBox8.Text=="0" & textBox5.Text!="0")
+            {
+                textBox8.Text = String.Empty;
+            }
+            if (textBox5.Text=="0")
+            {
+                textBox5.Text = String.Empty;
+                textBox6.Text = String.Empty;
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = "-";
+            input();
+
+            if ((first.denominator == 0) && (second.denominator == 0))
+            {
+                MessageBox.Show("Delete zeros from denominators.");
+            }
+            else
+            {
+                first.subtract(first.numerator, first.denominator, second.numerator, second.denominator);
+                textBox5.Text = first.newnum.ToString();
+                textBox6.Text = first.newdenum.ToString();
+                textBox8.Text = first.bignum.ToString();
+            }
+            clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = "+";
+            input();
+            if ((first.denominator == 0) && (second.denominator == 0))
+            {
+                MessageBox.Show("Delete zeros from denominators.");
+            }
+            else
+            {
+                first.add(first.numerator, first.denominator, second.numerator, second.denominator);
+                textBox5.Text = first.newnum.ToString();
+                textBox6.Text = first.newdenum.ToString();
+                textBox8.Text = first.bignum.ToString();
+            }
+            clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = "*";
+            input();
+            if ((first.denominator == 0) && (second.denominator == 0))
+            {
+                MessageBox.Show("Delete zeros from denominators.");
+            }
+            else
+            {
+                first.multi(first.numerator, first.denominator, second.numerator, second.denominator);
+                textBox5.Text = first.newnum.ToString();
+                textBox6.Text = first.newdenum.ToString();
+                textBox8.Text = first.bignum.ToString();
+            }
+            clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = "/";
+            input();
+            if ((first.denominator == 0) && (second.denominator == 0))
+            {
+                MessageBox.Show("Delete zeros from denominators.");
+            }
+            else
+            {
+                first.divis(first.numerator, first.denominator, second.numerator, second.denominator);
+                textBox5.Text = first.newnum.ToString();
+                textBox6.Text = first.newdenum.ToString();
+                textBox8.Text = first.bignum.ToString();
+            }
+            clear();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
